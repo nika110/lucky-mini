9763a8453d6b18d6ce74dcce0e956b3ce1f6a0ca
nika110 <nika.asatiani.1@btu.edu.ge>
2024-10-21 22:44:37 +0400

    nd

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	 build: {
+    outDir: '../docs'  // This puts the build output in the docs folder
+  },
   plugins: [react()]
 });
 
